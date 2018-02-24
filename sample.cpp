eb192d74d8fad1b9a7b5902f3c841c4d9a4e125b
yxl <kfyinxiaolei@126.com>
2018-02-24 23:27:29 +0800

    t2

--- sample.cpp
+++ sample.cpp
@@ -1,11 +1,8 @@
 #include<stdio.h>
 int main()
 {
-<<<<<<< HEAD
 	printf("testgit!ttttt22222222!");
-=======
 	printf("testgit!ttttt!");
         getchar();
->>>>>>> 09b37f2dcb8bebe30febdae29eec8da413b94417
 	return 0;
 }
